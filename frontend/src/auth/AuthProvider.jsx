8464d4f5c622f8aeaed2ace09e58900d690de70e
Justin <59674603+Toash@users.noreply.github.com>
2025-03-17 23:43:38 -0400

    fix sign up bug
    didnt have notRequired in the route- redirected the user to sign in when going to sign up

--- frontend/src/auth/AuthProvider.jsx
+++ frontend/src/auth/AuthProvider.jsx
@@ -27,7 +27,6 @@ export default function Auth({ children, notRequired }) {
         setLoading(false);
       }
     }
-
     run();
   }, [notRequired]);
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
   {
     path: '/signup',
     element:
-      <Auth>
+      <Auth notRequired>
         <SignupPage />
       </Auth>
   },
