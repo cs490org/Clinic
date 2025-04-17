fc4a0239ecd46b940214bb07fbe85b186c5e9c6a
Justin <59674603+Toash@users.noreply.github.com>
2025-04-17 15:46:49 -0400

    add unique to doctor license
    # Conflicts:
    #	backend/src/main/java/com/cs490/group4/demo/dao/Doctor.java

--- backend/src/main/java/com/cs490/group4/demo/dao/Doctor.java
+++ backend/src/main/java/com/cs490/group4/demo/dao/Doctor.java
@@ -11,6 +11,8 @@ import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
