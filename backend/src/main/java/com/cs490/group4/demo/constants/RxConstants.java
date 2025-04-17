270e70846de1d4aa75191db28bf310552a67890e
BryceBlankinship <blankinshipbusiness@gmail.com>
2025-04-17 15:41:41 -0400

    added ability to create prescriptions assigned between doctors, patients, a drug, etc

---
+++ backend/src/main/java/com/cs490/group4/demo/constants/RxConstants.java
@@ -0,0 +1,7 @@
+package com.cs490.group4.demo.constants;
+
+public class RxConstants {
+
+    public static final Integer NEW_PRESCRIPTION = 0;
+
+}
--- backend/src/main/java/com/cs490/group4/demo/dao/RxStatusCode.java
+++ backend/src/main/java/com/cs490/group4/demo/dao/RxStatusCode.java
@@ -3,10 +3,18 @@ package com.cs490.group4.demo.dao;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RxStatusCode {
 
     @Id
