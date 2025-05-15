e65fae4f0060f347f6b8fc322bc90830de4317cf
BryceBlankinship <blankinshipbusiness@gmail.com>
2025-05-14 20:46:21 -0400

    update backend urls

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -2,5 +2,5 @@
 // export const PHARMACY_API_URL = "http://localhost:8081";
 export const APP_BAR_HEIGHT= "64px" // used to shift other elements based off height of app bar
 
-export const API_URL = "https://clinic-919309081290.us-east4.run.app";
-export const PHARMACY_API_URL = "https://pharmacy-919309081290.us-central1.run.app"
+export const API_URL = "https://clinic.bryceblankinship.com";
+export const PHARMACY_API_URL = "https://pharmacy.bryceblankinship.com"
